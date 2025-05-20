fc28a1e39bf32962c5ec15fc2c4b263ca47d6b95
Arsene-Baitenov <arsenebaitenov@gmail.com>
2025-05-20 06:58:40 +0300

    Configure app for choosing ui

--- Library/app/src/main/java/library/App.java
+++ Library/app/src/main/java/library/App.java
@@ -1,54 +1,29 @@
 package library;
 
-import library.model.Book;
-import library.model.Library;
-import library.model.Magazine;
-import library.model.Publication;
+import java.util.Arrays;
 
-import java.util.Scanner;
+import library.application.ConsoleApp;
+import library.application.GuiLauncher;
+
 
 public class App {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Library<Publication> library = new Library<>();
+        String[] appArgs = Arrays.stream(args, 1, args.length).toArray(String[]::new);
 
-        while (true) {
-            System.out.println("1. Add Book\n2. Add Magazine\n3. Show All\n4. Exit");
-            int choice = scanner.nextInt();
-            scanner.nextLine();
-
-            switch (choice) {
-                case 1 -> {
-                    System.out.print("Title: ");
-                    String title = scanner.nextLine();
-                    System.out.print("Author: ");
-                    String author = scanner.nextLine();
-                    System.out.print("Year: ");
-                    int year = scanner.nextInt();
-                    scanner.nextLine();
-                    System.out.print("Genre: ");
-                    String genre = scanner.nextLine();
-                    library.addPublication(new Book(title, author, year, genre));
-                }
-                case 2 -> {
-                    System.out.print("Title: ");
-                    String title = scanner.nextLine();
-                    System.out.print("Author: ");
-                    String author = scanner.nextLine();
-                    System.out.print("Year: ");
-                    int year = scanner.nextInt();
-                    System.out.print("Issue Number: ");
-                    int issue = scanner.nextInt();
-                    scanner.nextLine();
-                    library.addPublication(new Magazine(title, author, year, issue));
-                }
-                case 3 -> library.forEach(p -> System.out.println(p.getInfo()));
-                case 4 -> {
-                    System.out.println("Bye!");
-                    return;
-                }
-                default -> System.out.println("Invalid option.");
-            }
+        if (args.length > 0 && args[0].equalsIgnoreCase("cli")) {
+            runConsole(appArgs);
+        } else if (args.length > 0 && args[0].equalsIgnoreCase("gui")) {
+            runGui(appArgs);
+        } else {
+            System.out.println("Incorrect cli arguments (choice cli | gui)");
         }
+    }
+
+    private static void runConsole(String[] args) {
+        ConsoleApp.run(args);
+    }
+
+    private static void runGui(String[] args) {
+        GuiLauncher.run(args);
     }
 }
